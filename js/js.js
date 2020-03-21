973105251829ce929ddbca7a96f9004520f4444e
Rake Runner <54960097+Rake-Runner@users.noreply.github.com>
2020-03-22 01:00:12 +0300

    2

--- js/js.js
+++ js/js.js
@@ -1,50 +1,25 @@
-var openMenu = function openMenu() {
-    document.getElementById("menu_item").style.display = "block";
-    document.getElementById("About_author").style.display = "none";
-    document.getElementById("menu_link").style.display = "none";
-  };
-  
-  var closeMenu = function closeMenu() {
-    document.getElementById("menu_item").style.display = "none";
-    document.getElementById("About_author").style.display = "block";
-    document.getElementById("menu_link").style.display = "block";
-  };
-  
-  var chapterOne = function chapterOne() {
-    closeMenu();
-    document.getElementById("chapter_why").style.display = "none";
-    document.getElementById("chapter_one").style.display = "block";
-  };
-  
-  var chapterWhy = function chapterWhy() {
-    closeMenu();
-    document.getElementById("chapter_why").style.display = "block";
-    document.getElementById("chapter_one").style.display = "none";
-  };
-  var chapterTwo = function chapterTwo(){
-    closeMenu();
-    document.getElementById("chapter_why").style.display = "none";
-    document.getElementById("chapter_one").style.display = "none";
-    document.getElementById("chapter_two").style.display = "block";
+let dark = document.getElementById("dark");
+let white = document.getElementById("white");
+let header = document.getElementById("theheader");
+let bg = document.getElementById("thebg");
 
-  }
-  var myApp = function myApp(){
-    closeMenu();
-    document.getElementById("chapter_why").style.display = "none";
-    document.getElementById("chapter_one").style.display = "none";
-    document.getElementById("chapter_two").style.display = "none";
 
-  }
-  
-  var menuIcon = document.getElementById("menu_link");
-  var menuBack = document.getElementById("back_menu");
-  var chWhy = document.getElementById("why");
-  var chOne = document.getElementById("ch1");
-  var chTwo = document.getElementById("ch2");
-  var myAppLink = document.getElementById("app1");
-  menuIcon.addEventListener("click", openMenu);
-  menuBack.addEventListener("click", closeMenu);
-  chWhy.addEventListener("click", chapterWhy);
-  chOne.addEventListener("click", chapterOne);
-  chTwo.addEventListener("click", chapterTwo);
-  myAppLink.addEventListener("click", myApp);
+let darkon = function(){
+  header.style.color = "white";
+  header.style.backgroundColor = "black";
+  bg.style.backgroundColor = "black";
+  bg.style.color = "white";
+  dark.style.display = "none";
+  white.style.display = "block";
+};
+let darkoff = function(){
+  header.style.color = "black";
+  header.style.backgroundColor = "white";
+  bg.style.backgroundColor = "white";
+  bg.style.color = "black";
+  white.style.display = "none";
+  dark.style.display = "block";
+};
+
+dark.addEventListener("click", darkon);
+white.addEventListener("click", darkoff);
